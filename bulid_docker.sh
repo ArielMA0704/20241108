2f99286d4590356df5d725e41e9b1a768674c648
Edward Lo <edward70932@gmail.com>
2024-07-15 06:10:08 +0000

    Generate 2.0.0 Image

--- bulid_docker.sh
+++ bulid_docker.sh
@@ -1,2 +1,2 @@
-docker build -f Dockerfile.prod -t bdlai.azurecr.io/smart-doc/web:0.0.3 .
-docker push bdlai.azurecr.io/smart-doc/web:0.0.3
+docker build -f Dockerfile.prod -t bdlai.azurecr.io/smart-doc/web:2.0.0 .
+docker push bdlai.azurecr.io/smart-doc/web:2.0.0
